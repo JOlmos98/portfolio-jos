84e791ed6358d6c00056ddf1d514e0ed948d5519
JOlmos98 <soler98@hotmail.es>
2024-12-21 12:01:50 +0100

    feat: ThemeButton terminado y sin warnings, al iniciar la web, si tenemos el tema oscuro por defecto, primero muestra el tema claro antes de pasar al dark, queda mal, si le quito el transition creo que no ocurre. 21/12/24

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export {Navbar} from './navbar/Navbar';
 export {ThemeButton} from './ThemeButton/ThemeButton';
+export {Providers} from './ThemeButton/Providers';
 export {LanguageSelector} from './LanguageSelector/LanguageSelector';
