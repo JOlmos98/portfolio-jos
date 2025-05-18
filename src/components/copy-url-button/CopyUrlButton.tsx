07ece41c2cc1ae78acac685f7faf5d9ab7780f6a
JOlmos98 <soler98@hotmail.es>
2025-05-18 13:53:45 +0200

    fix: articles cards responsive.

--- src/components/copy-url-button/CopyUrlButton.tsx
+++ src/components/copy-url-button/CopyUrlButton.tsx
@@ -30,18 +30,3 @@ export const CopyUrlButton = ({ url }: { url: string }) => {
 
   );
 };
-
-// "use client";
-
-// import React from 'react'
-// import { IoLinkOutline } from 'react-icons/io5';
-
-// export const CopyUrlButton = () => {
-//   return (
-//     <div>
-//       <IoLinkOutline className="text-xl mx-2" />
-//     </div>
-//   )
-// }
-
-
