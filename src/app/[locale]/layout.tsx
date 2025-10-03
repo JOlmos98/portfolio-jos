c21abee3ee5d4ed8c46dc28ad0dd99fe2ca2b760
JOlmos98 <soler98@hotmail.es>
2025-10-03 22:24:18 +0200

    try

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -97,8 +97,6 @@ export default async function RootLayout({
     notFound();
   }
 
-  // Providing all messages to the client
-  // side is the easiest way to get started
   const messages = await getMessages();
 
   return (
